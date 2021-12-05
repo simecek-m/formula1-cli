644a127096eff20c84883d1896b9d8c98505bc30
Martin Šimeček <simecek.m@seznam.cz>
2021-12-05 14:03:57 +0100

    implement basic team scraping

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import dataFolder from "./src/data/folder.js"
 import { writeToCsvFile} from "./src/data/file.js"
 import { scrapeDriversFromYear } from "./src/page/driver.js"
+import { scrapeTeamsFromRange, scrapeTeamsFromYear } from "./src/page/team.js"
 
 async function start() {
   await dataFolder.initialize()
+
   const drivers = await scrapeDriversFromYear(2020)
   await writeToCsvFile("drivers", drivers)
+
+  const teams = await scrapeTeamsFromYear(2020)
+  await writeToCsvFile("teams", teams)
 }
 
 start()
---
+++ src/page/team.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+import cheerio from "cheerio"
+import logger from "../logger/index.js"
+import { RESULTS_URL } from '../constant/url.js'
+import { CHAMPIONSHIP_FIRST_YEAR, THIS_YEAR } from '../constant/year.js'
+
+const getUrlByYear = year => {
+  return `${RESULTS_URL}/${year}/team.html`
+}
+
+export async function scrapeTeamsFromYear(year = THIS_YEAR) {
+  logger.info(`Scrapping teams from ${year}`)
+  const teams = []
+  const url = getUrlByYear(year)
+  try {
+    const response = await fetch(url)
+    const body = await response.text()
+    const $ = cheerio.load(body)
+    $(".resultsarchive-table tbody tr").each((_, row) => {
+      const name = $(row).find("td:nth-child(3) a").text()
+      teams.push({ name })
+    })
+  } catch (error) {
+    logger.error(`Error while srapping teams from year: ${year}`)
+  } finally {
+    return teams
+  }
+}
+
+export async function scrapeTeamsFromRange(from = CHAMPIONSHIP_FIRST_YEAR, to = THIS_YEAR) {
+  let teams = [];
+  for(let year = from; year <= to; year++) {
+    const yearTeams = await scrapeTeamsFromYear(year)
+    teams = [...teams, ...yearTeams]
+  }
+  return teams
+}
