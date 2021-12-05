bbf0a1a4b135ff2fe1ac56407154bfca55ed756d
Martin Šimeček <simecek.m@seznam.cz>
2021-12-05 13:30:22 +0100

    add headers to drivers.csv files

--- src/data/file.js
+++ src/data/file.js
@@ -6,7 +6,7 @@ import { resolve } from "path"
 export async function writeToCsvFile(fileName, data) {
   logger.info(`Writing data to ${fileName}.csv`)
   const filePath = resolve(FOLDER_PATH, `${fileName}.csv`)
-  writeToPath(filePath, data)
+  writeToPath(filePath, data, { headers: true })
     .on("error", err => logger.error(`Writing ${fileName}.csv file caused error: `, err))
     .on("finish", () => logger.info(`${fileName}.csv was written successfully`))
 }
