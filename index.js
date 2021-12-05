b5e81ccc9d5a6d148bd02e71966b8f4a881435e8
Martin Šimeček <simecek.m@seznam.cz>
2021-12-05 14:12:41 +0100

    move season from hardcoded value to variable

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import { writeToCsvFile} from "./src/data/file.js"
 import { scrapeDriversFromYear } from "./src/page/driver.js"
 import { scrapeTeamsFromRange, scrapeTeamsFromYear } from "./src/page/team.js"
 
+const SEASON = 2015;
+
 async function start() {
   await dataFolder.initialize()
 
-  const drivers = await scrapeDriversFromYear(2020)
+  const drivers = await scrapeDriversFromYear(SEASON)
   await writeToCsvFile("drivers", drivers)
 
-  const teams = await scrapeTeamsFromYear(2020)
+  const teams = await scrapeTeamsFromYear(SEASON)
   await writeToCsvFile("teams", teams)
 }
 
