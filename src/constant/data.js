8edee2bc03a74010fad1dcedf7e57a1ea161bf23
Martin Šimeček <simecek.m@seznam.cz>
2021-12-12 11:12:56 +0100

    all constants rewrite to TS

--- src/constant/data.js
+++
@@ -1,3 +0,0 @@
-import { join, dirname } from "path"
-
-export const FOLDER_PATH = join(dirname("."), "/data/")
---
+++ src/constant/data.ts
@@ -0,0 +1,4 @@
+import { join, dirname } from "path"
+
+const FOLDER_NAME = "/data";
+export const FOLDER_PATH = join(dirname("."), FOLDER_NAME)
